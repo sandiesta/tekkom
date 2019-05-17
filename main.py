3d592968e2ce01a8498d217a3b2a2897d78f6acb
sandiesta <44359182+sandiesta@users.noreply.github.com>
2019-05-17 11:25:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import lexer
-import parser
-import interpreter
+import san_lexer
+import san_parser
+import san_interpreter
 
 from sys import *
 
-lexer = lexer.BasicLexer()
-parser = parser.BasicParser()
+#DENGAN MASUKAN BAHASAKU.san
+lexer = san_lexer.BasicLexer()
+parser = san_parser.BasicParser()
 env = {}
 
 file = open(argv[1])
 text = file.readlines()
 for line in text:
     tree = parser.parse(lexer.tokenize(line))
-    interpreter.BasicExecute(tree, env)
+    san_interpreter.BasicExecute(tree, env)
