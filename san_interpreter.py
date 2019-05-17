3f585799b7e2551f59dfa69a35ab21f77a8dfbd7
sandiesta <44359182+sandiesta@users.noreply.github.com>
2019-05-17 11:26:47 +0700

    Update and rename interpreter.py to san_interpreter.py

--- san_interpreter.py
+++ san_interpreter.py
@@ -1,5 +1,5 @@
-import rhs_lexer
-import rhs_parser
+import san_lexer
+import san_parser
 
 class BasicExecute:
 
@@ -95,8 +95,8 @@ class BasicExecute:
 
 
 if __name__ == '__main__':
-    lexer = rhs_lexer.BasicLexer()
-    parser = rhs_parser.BasicParser()
+    lexer = san_lexer.BasicLexer()
+    parser = san_parser.BasicParser()
     env = {}
     while True:
         try:
